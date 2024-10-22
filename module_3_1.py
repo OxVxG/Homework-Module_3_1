ca0a94b408ff0c70610e1e3f44b18a50f4d3604b
Oleg Gordyushin <gord.oleg@gmail.com>
2024-10-22 16:50:10 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,51 @@
+calls = 0
+
+def count_calls():
+    global calls
+    calls += 1
+    return calls
+
+def string_info(string):
+    count_calls()
+    return len(string), str(string).upper(), str(string).lower()
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for item in list_to_search:
+        if string.casefold() == item.casefold():
+            return True
+    else:
+            return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
+
+
+
+
+
+# # calls = 0
+# #
+# # def count_calls():
+# #     global calls
+# #     calls += 1
+# #     return calls
+#
+# def string_info(string):
+#
+#     return len(string), str(string).upper(), str(string).lower()
+#
+#     # global calls
+#         # count_calls()
+#
+#         # def is_contains(string, list_to_search):
+#         #     if string == list_to_search:
+#         #         string = True
+#         #     else:
+#         #         string = False
+#
+#
+# print(string_info('Capybara'))
